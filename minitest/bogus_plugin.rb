0fb14d922557dd803c5c9afe0fc4b830eb7b4929
sivagollapalli <siva@joshsoftware.com>
2014-02-28 23:13:25 +0530

    Modified plugin

--- minitest/bogus_plugin.rb
+++ minitest/bogus_plugin.rb
@@ -2,15 +2,9 @@
 
 module Minitest
   def self.plugin_bogus_options(opts, options)
-    opts.on "--myci", "Report results to my CI" do
-      options[:myci] = true
-      options[:myci_addr] = get_myci_addr
-      options[:myci_port] = get_myci_port
-    end
   end
 
   def self.plugin_bogus_init(options)
-    p "fdsfdsdfsfsfdsfsdffds"
-    #self.reporter << MyCI.new(options) if options[:myci]
+    p "My first plugin"
   end
 end
